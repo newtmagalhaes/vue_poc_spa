0608c0da57ead640ddeb79355bb80883fbcbc9cd
Anilton Magalhães <anilton.magalhaes@irede.org.br>
2023-05-03 10:38:58 -0300

    feat: add axios, json-server, primevue and primeicons

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+//theme
+import "primevue/resources/themes/soho-dark/theme.css";     
+
+//core
+import "primevue/resources/primevue.min.css";
+import 'primeicons/primeicons.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
