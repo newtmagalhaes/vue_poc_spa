c0698e2e55a8117ae865903ab1ae8b1e88f00275
Anilton Magalhães WSL <anilton.magalhaes@irede.org.br>
2023-05-08 23:43:35 -0300

    feat: remove jobForm's props

--- src/components/JobForm.vue
+++ src/components/JobForm.vue
@@ -8,10 +8,23 @@ import Textarea from 'primevue/textarea'
 import Toast from 'primevue/toast';
 import { useToast } from 'primevue/usetoast';
 
-import { JobPost, postJob } from '@/service/http/Job';
+import { Dificulty, Job, JobPost, postJob } from '@/service/http/Job';
 import router from '@/router';
+import { inject, onMounted, reactive } from 'vue';
 
-const props = defineProps<{ job: JobPost }>();
+// const props = defineProps<{ job: JobPost }>();
+const data = reactive<JobPost>({
+    title: '',
+    dificulty: Dificulty.easy,
+    description: '',
+})
+
+const {job: teste} = inject<{job: JobPost}>("meudado", {job: data})
+// onMounted(() => {
+//     // const injectedJob: Job = teste?.job || {};
+//     console.log(teste);
+//     // data.title = injectedJob.title || '';
+// })
 
 const toast = useToast()
 
@@ -24,11 +37,11 @@ function adjustData(data: JobPost): JobPost {
 }
 
 const onSubmit = (values: Event) => {
-    postJob(adjustData(props.job))
-        .then((response) => {
-            router.push({path: "/"}).then((response)=>{
-                console.log("analise")
-                toast.add({ severity: 'info', summary: 'Form Submitted', detail: props.job.title, life: 3000 });
+    postJob(adjustData(data))
+    .then((response) => {
+        router.push({path: "/"}).then((response)=>{
+            console.log("analise")
+            toast.add({ severity: 'info', summary: 'Form Submitted', detail: data.title, life: 3000 });
             })
             .catch((error) => {
                 console.log(error)
@@ -43,7 +56,6 @@ const onSubmit = (values: Event) => {
             })
         })
 }
-
 </script>
 
 
@@ -54,17 +66,17 @@ const onSubmit = (values: Event) => {
                 <label for="title">Título</label>
             </div>
             <div class="field">
-                <InputText id="title" type="text" v-model="props.job.title" />
+                <InputText id="title" type="text" v-model="data.title" />
             </div>
             <div class="field">
                 <label for="dificuldade">Dificuldade</label>
-                <SelectButton id="dificuldade" v-model="props.job.dificulty" :options="['easy', 'medium', 'hard']" />
+                <SelectButton id="dificuldade" v-model="data.dificulty" :options="['easy', 'medium', 'hard']" />
             </div>
             <div class="field">
                 <label for="description">Descrição</label>
             </div>
             <div class="field">
-                <Textarea id="description" v-model="props.job.description" auto-resize />
+                <Textarea id="description" v-model="data.description" auto-resize />
             </div>
             <Button label="Criar" v-on:click="onSubmit" />
         </form>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
 import JobForm from '@/components/JobForm.vue';
-import { Dificulty, JobPost } from '@/service/http/Job';
-import { reactive } from 'vue';
-
-const data = reactive<JobPost>({
-    title: '',
-    dificulty: Dificulty.easy,
-    description: '',
-})
 
 </script>
 
 <template>
-    <JobForm :job="data"/>
+    <JobForm />
 </template>
